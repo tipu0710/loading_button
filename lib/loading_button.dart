d74c3b563efd234cf23952b2c8de1449820ff3a9
Tipu Sultan <tsultan0710@gmail.com>
2021-04-15 21:05:38 +0600

    alignment fixed

--- lib/loading_button.dart
+++ lib/loading_button.dart
@@ -42,63 +42,67 @@ class _LoadingButtonState extends State<LoadingButton> {
 
   @override
   Widget build(BuildContext context) {
-    return Stack(
-      children: [
-        Center(
-          child: AnimatedOpacity(
-            duration: widget.animationDuration ?? Duration(milliseconds: 500),
-            opacity: _loading ? 1 : 0,
-            child: Center(
-              child: Container(
-                margin: widget.margin ?? EdgeInsets.all(0),
-                height: widget.loaderSize ?? 30,
-                width: widget.loaderSize ?? 30,
-                child: CircularProgressIndicator(),
-              ),
-            ),
-          ),
-        ),
-        Center(
-          child: AnimatedContainer(
-            margin: widget.margin ?? EdgeInsets.all(0),
-            duration: widget.animationDuration ?? Duration(milliseconds: 500),
-            height: _loading ? 0 : widget.buttonHeight,
-            width: _loading ? 0 : widget.buttonWidth,
-            child: Center(
-              child: ElevatedButton(
-                onPressed: () async {
-                  toggleLoading();
-                  await widget.onPress();
-                  toggleLoading();
-                },
-                style: ElevatedButton.styleFrom(
-                  primary: widget.buttonColor ?? Theme.of(context).primaryColor,
-                  onPrimary: widget.buttonColor ?? Theme.of(context).primaryColor,
-                  shape: RoundedRectangleBorder(
-                    borderRadius:
-                    BorderRadius.circular(widget.buttonCorner ?? 0.0),
-                  ),
-                ),
-                child: Center(
-                  child: Text(
-                    widget.buttonTitle,
-                    style: widget.buttonTextStyle ??
-                        TextStyle(
-                          fontFamily: 'Robot',
-                          fontSize: 15,
-                          color: Color(0xffffffff),
-                          fontWeight: FontWeight.w500,
-                        ),
-                    textHeightBehavior:
-                    TextHeightBehavior(applyHeightToFirstAscent: false),
-                    textAlign: TextAlign.center,
-                  ),
+    return SizedBox(
+      height: widget.buttonHeight+1,
+      width: widget.buttonWidth+1,
+      child: Stack(
+        children: [
+          Center(
+            child: AnimatedOpacity(
+              duration: widget.animationDuration ?? Duration(milliseconds: 500),
+              opacity: _loading ? 1 : 0,
+              child: Center(
+                child: Container(
+                  margin: widget.margin ?? EdgeInsets.all(0),
+                  height: widget.loaderSize ?? 30,
+                  width: widget.loaderSize ?? 30,
+                  child: CircularProgressIndicator(),
                 ),
               ),
             ),
           ),
-        ),
-      ],
+          Center(
+            child: AnimatedContainer(
+              margin: widget.margin ?? EdgeInsets.all(0),
+              duration: widget.animationDuration ?? Duration(milliseconds: 500),
+              height: _loading ? 0 : widget.buttonHeight,
+              width: _loading ? 0 : widget.buttonWidth,
+              child: Center(
+                child: ElevatedButton(
+                  onPressed: () async {
+                    toggleLoading();
+                    await widget.onPress();
+                    toggleLoading();
+                  },
+                  style: ElevatedButton.styleFrom(
+                    primary: widget.buttonColor ?? Theme.of(context).primaryColor,
+                    onPrimary: widget.buttonColor ?? Theme.of(context).primaryColor,
+                    shape: RoundedRectangleBorder(
+                      borderRadius:
+                      BorderRadius.circular(widget.buttonCorner ?? 0.0),
+                    ),
+                  ),
+                  child: Center(
+                    child: Text(
+                      widget.buttonTitle,
+                      style: widget.buttonTextStyle ??
+                          TextStyle(
+                            fontFamily: 'Robot',
+                            fontSize: 15,
+                            color: Color(0xffffffff),
+                            fontWeight: FontWeight.w500,
+                          ),
+                      textHeightBehavior:
+                      TextHeightBehavior(applyHeightToFirstAscent: false),
+                      textAlign: TextAlign.center,
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
